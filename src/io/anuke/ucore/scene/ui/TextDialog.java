9f14072862c6bba9ca2bb7a9b3702c7283074a0f
Anuken <arnukren@gmail.com>
2017-06-05 00:11:29 -0400

    reverted everything

--- src/io/anuke/ucore/scene/ui/TextDialog.java
+++ src/io/anuke/ucore/scene/ui/TextDialog.java
@@ -1,27 +1,12 @@
 package io.anuke.ucore.scene.ui;
 
-import io.anuke.ucore.scene.Element;
-import io.anuke.ucore.scene.ui.layout.Value;
-
 public class TextDialog extends Dialog{
-	private float textwidth = -1;
 	
-	private Value value = new Value(){
-		public float get(Element e){
-			return textwidth <= 0 ? e.getPrefWidth() : textwidth;
-		}
-	};
-	
-	public TextDialog(String title, float width, String... text) {
+	public TextDialog(String title, String... text) {
 		super(title);
-		this.textwidth = width;
 		addCloseButton();
 		
 		set(title, text);
-	}
-	
-	public TextDialog(String title, String... text) {
-		this(title, -1, text);
 	}
 	
 	public void set(String title, String... text){
@@ -31,13 +16,9 @@ public class TextDialog extends Dialog{
 		for(String s : text){
 			Label label = new Label(s);
 			label.setWrap(true);
-			content().add(label).width(value);
+			content().add(label);
 			content().row();
 		}
-	}
-	
-	public void setTextWidth(float w){
-		this.textwidth = w;
 	}
 	
 	public TextDialog padText(float amount){
