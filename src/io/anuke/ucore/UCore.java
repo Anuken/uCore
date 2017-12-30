9d26472111b63897fe7cdcf5776e98ef4b841106
Anuken <arnukren@gmail.com>
2017-12-29 22:25:36 -0500

    Added isAssets for checking some things

--- src/io/anuke/ucore/UCore.java
+++ src/io/anuke/ucore/UCore.java
@@ -2,10 +2,17 @@ package io.anuke.ucore;
 
 import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 import com.badlogic.gdx.utils.reflect.Field;
+import com.badlogic.gdx.utils.reflect.Method;
+import com.badlogic.gdx.utils.reflect.ReflectionException;
 
 public class UCore{
+	public static final boolean isAssets =
+			Gdx.app.getType() != ApplicationType.WebGL
+					&& getProperty("user.name").equals("anuke")
+					&& getAbsolute(Gdx.files.local("na").parent()).endsWith("assets");
 	
 	public static void log(Object...objects){
 		int i = 0;
@@ -16,15 +23,34 @@ public class UCore{
 		}
 		System.out.println();
 	}
+
+	public static String getProperty(String name){
+		try{
+			Method method = ClassReflection.getMethod(System.class, "getProperty", String.class);
+			return (String)method.invoke(null, name);
+		}catch(ReflectionException e){
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String getAbsolute(FileHandle file){
+		try{
+			Method method = ClassReflection.getMethod(file.getClass(), "file");
+			Object object = method.invoke(file);
+			Method fm = ClassReflection.getMethod(object.getClass(), "getAbsolutePath");
+			return (String)fm.invoke(object);
+		}catch(ReflectionException e){
+			throw new RuntimeException(e);
+		}
+	}
 	
 	public static Object getPrivate(Object object, String name){
 		try{
 			Field field = ClassReflection.getDeclaredField(object.getClass(), name);
 			field.setAccessible(true);
 			return field.get(object);
-		}catch(Exception e){
-			e.printStackTrace();
-			return null;
+		}catch(ReflectionException e){
+            throw new RuntimeException(e);
 		}
 	}
 }
