3b08595f74f125f776405e926365b49721d49f4f
Anuken <arnukren@gmail.com>
2018-01-31 23:38:29 -0500

    Fixed HTML5 bug

--- src/io/anuke/ucore/core/Timers.java
+++ src/io/anuke/ucore/core/Timers.java
@@ -13,7 +13,7 @@ public class Timers{
 	private static ObjectFloatMap<Integer> timers = new ObjectFloatMap<>();
 	private static DelayedRemovalArray<DelayRun> runs = new  DelayedRemovalArray<>();
 	private static long lastMark = 0;
-	private static Supplier<Float> deltaimpl = ()->Math.min(Gdx.graphics.getDeltaTime()*60f, 3f);
+	private static Supplier<Float> deltaimpl = () -> Math.min(Gdx.graphics.getDeltaTime()*60f, 3f);
 	
 	public static void run(float delay, Callable r){
 		DelayRun run = Pools.obtain(DelayRun.class);
