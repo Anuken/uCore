aa2e294b107c659033b92b3defa47bae75884d50
Anuken <arnukren@gmail.com>
2018-04-12 17:17:09 -0400

    Removed ecs package

--- src/io/anuke/ucore/ecs/BasisListener.java
+++
@@ -1,6 +0,0 @@
-package io.anuke.ucore.ecs;
-
-public interface BasisListener{
-	public void added(Spark spark);
-	public void removed(Spark spark);
-}
--- src/io/anuke/ucore/ecs/SparkEvent.java
+++
@@ -1,5 +0,0 @@
-package io.anuke.ucore.ecs;
-
-public interface SparkEvent{
-
-}
