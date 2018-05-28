4db79ab343221924d30f2d8da446ef872851eeb4
Anuken <arnukren@gmail.com>
2018-05-28 12:30:51 -0400

    Removed unneeded BulletType id storage

--- src/io/anuke/ucore/entities/BaseBulletType.java
+++ src/io/anuke/ucore/entities/BaseBulletType.java
@@ -1,16 +1,11 @@
 package io.anuke.ucore.entities;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
 import io.anuke.ucore.core.Effects;
 import io.anuke.ucore.core.Effects.Effect;
 
 /**Presumably you would extends BulletType and put in the default bullet entity type.*/
 public abstract class BaseBulletType<T extends BulletEntity>{
-	private static int lastid = 0;
-	private static Array<BaseBulletType> types = new Array<>();
-
-	public final int id = lastid ++;
 	public float lifetime = 100;
 	public float speed = 1f;
 	public float damage = 1;
@@ -23,10 +18,10 @@ public abstract class BaseBulletType<T extends BulletEntity>{
 	protected Vector2 vector = new Vector2();
 
 	public BaseBulletType(){
-		types.add(this);
+
 	}
 	
-	public abstract void draw(T b);
+	public void draw(T b){}
 	public void init(T b){}
 	public void update(T b){}
 
@@ -42,9 +37,5 @@ public abstract class BaseBulletType<T extends BulletEntity>{
 	public void despawned(T b){
 		if(despawneffect != null)
 			Effects.effect(despawneffect, b.x, b.y, b.angle());
-	}
-
-	public static <T extends BaseBulletType<?>> T getByID(int id){
-		return (T)types.get(id);
 	}
 }
