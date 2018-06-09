f0adaffe15ca79d5fa842d763dd25db81fe0ebae
Anuken <arnukren@gmail.com>
2018-06-09 17:04:50 -0400

    Added ThreadLocal emulation

---
+++ src/emu/java/lang/ThreadLocal.java
@@ -0,0 +1,17 @@
+package java.lang;
+
+public class ThreadLocal<T> {
+  private T value;
+
+  public T get() {
+    return value;
+  }
+
+  public void set(T value) {
+    this.value = value;
+  }
+
+  public void remove() {
+    value = null;
+  }
+}
