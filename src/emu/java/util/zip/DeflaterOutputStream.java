bc1c2e0848cce8f61546c2d61bde9ee3f36a7654
Anuken <arnukren@gmail.com>
2018-07-03 11:21:24 -0400

    Added emulation of compressed streams

---
+++ src/emu/java/util/zip/DeflaterOutputStream.java
@@ -0,0 +1,16 @@
+package java.util.zip;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class DeflaterOutputStream extends OutputStream{
+
+    public DeflaterOutputStream(OutputStream stream){
+        //stub
+    }
+
+    @Override
+    public void write(int i) throws IOException {
+
+    }
+}
---
+++ src/emu/java/util/zip/InflaterOutputStream.java
@@ -0,0 +1,16 @@
+package java.util.zip;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InflaterOutputStream extends InputStream{
+
+    public InflaterOutputStream(InputStream stream){
+        //stub
+    }
+
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
+}
