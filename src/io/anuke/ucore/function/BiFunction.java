91fb3eb182bebacdfcdbb7d979eee3c15a1f2ecc
Anuken <arnukren@gmail.com>
2018-09-01 14:43:17 -0400

    Added functional functions

---
+++ src/io/anuke/ucore/function/BiFunction.java
@@ -0,0 +1,5 @@
+package io.anuke.ucore.function;
+
+public interface BiFunction<P1, P2, R>{
+    R get(P1 param1, P2 param2);
+}
---
+++ src/io/anuke/ucore/function/Function.java
@@ -0,0 +1,5 @@
+package io.anuke.ucore.function;
+
+public interface Function<P, R>{
+    R get(P param);
+}
