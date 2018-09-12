74dc653bbd66d1e8b10e22efb4f1206195674dd5
Anuken <arnukren@gmail.com>
2018-09-12 10:06:19 -0400

    apparently GWT is quite unintelligent

--- src/io/anuke/ucore/util/OSEmu.java
+++ src/io/anuke/ucore/util/OSEmu.java
@@ -1,14 +1,21 @@
 package io.anuke.ucore.util;
 
+import com.badlogic.gdx.utils.reflect.ClassReflection;
+
 /**Class for getting system properties. Do not use directly.
  * Only for GWT emulation.*/
 class OSEmu {
 
     public static String getProperty(String name){
-        return System.getProperty(name);
+        try{
+            return (String) ClassReflection.getMethod(System.class, "getProperty", String.class).invoke(null, name);
+        }catch(Throwable e){
+            return null;
+        }
     }
 
     public static String getEnv(String name){
-        return System.getenv(name);
+        return null;
     }
+
 }
