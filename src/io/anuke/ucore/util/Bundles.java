4d5571b3efc30975e0c073f87fae9ff5cb2ae4d7
Anuken <arnukren@gmail.com>
2018-10-20 08:54:37 -0400

    Fixed bundle formatting crash

--- src/io/anuke/ucore/util/Bundles.java
+++ src/io/anuke/ucore/util/Bundles.java
@@ -3,6 +3,7 @@ package io.anuke.ucore.util;
 import io.anuke.ucore.core.Core;
 
 public class Bundles{
+    private static StringBuilder build = new StringBuilder();
 
     public static boolean enabled(){
         return Core.bundle != null;
@@ -32,8 +33,28 @@ public class Bundles{
         return s;
     }
 
-    public static String format(String key, Object... args){
-        return Core.bundle.format(key, args);
+    public static synchronized String format(String key, Object... args){
+        if(args.length > 9) throw new IllegalArgumentException("Only up to 9 arguments are allowed.");
+        String value = get(key);
+        if(value == null || value.startsWith("???")) return value;
+
+        build.setLength(0);
+
+        for(int i = 0; i < value.length(); i++){
+           char c = value.charAt(i);
+           if(i < value.length() - 2 && c == '{' && Character.isDigit(value.charAt(i + 1)) && value.charAt(i + 2) == '}'){
+               int argument = Character.getNumericValue(value.charAt(i + 1));
+               if(args.length >= argument){
+                   build.append(args[argument]);
+                   i += 2;
+               }else{
+                   build.append(c);
+               }
+           }else{
+               build.append(c);
+           }
+        }
+        return build.toString();
     }
 
 }
