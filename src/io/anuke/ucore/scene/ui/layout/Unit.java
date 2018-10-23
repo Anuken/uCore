eac024e83a95bdc45e614c8ab5d6a9c2d61fa70f
Arne <arnesacnussem@gmail.com>
2018-10-23 19:39:00 +0800

    Add a product of scl for scale settings.

--- src/io/anuke/ucore/scene/ui/layout/Unit.java
+++ src/io/anuke/ucore/scene/ui/layout/Unit.java
@@ -4,33 +4,34 @@ import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import io.anuke.ucore.util.Mathf;
 
-public enum Unit{
-    px{
+public enum Unit {
+    px {
         @Override
-        public float scl(float amount){
+        public float scl(float amount) {
             return amount;
         }
     },
-    dp{
+    dp {
         float scl = -1;
 
         @Override
-        public float scl(float amount){
-            if(scl < 0f){
+        public float scl(float amount) {
+            if (scl < 0f) {
                 //calculate scaling value if it hasn't been set yet
-                if(Gdx.app.getType() == ApplicationType.Desktop){
+                if (Gdx.app.getType() == ApplicationType.Desktop) {
+                    scl = 1f * product;
+                } else if (Gdx.app.getType() == ApplicationType.WebGL) {
                     scl = 1f;
-                }else if(Gdx.app.getType() == ApplicationType.WebGL){
-                    scl = 1f;
-                }else{
+                } else {
                     //mobile scaling
-                    scl = Math.max(Mathf.round2(Gdx.graphics.getDensity() / 1.5f + addition, 0.5f), 1f);
+                    scl = Math.max(Mathf.round2(Gdx.graphics.getDensity() / 1.5f + addition, 0.5f), 1f) * product;
                 }
             }
             return amount * scl;
         }
     };
     public float addition = 0f;
+    public float product = 1f;
 
     public abstract float scl(float amount);
 }
