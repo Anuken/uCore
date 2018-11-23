df6a6a0ad7c3bf3b6e51e9028aea109570b69528
Arne <arnesacnussem@gmail.com>
2018-11-23 22:09:41 +0800

    ConfirmDialog now can set cancel event.

--- src/io/anuke/ucore/scene/ui/ConfirmDialog.java
+++ src/io/anuke/ucore/scene/ui/ConfirmDialog.java
@@ -14,4 +14,18 @@ public class ConfirmDialog extends Dialog{
         buttons().addButton("Cancel", this::hide);
     }
 
+    public ConfirmDialog(String title, String text, Runnable confirm, Runnable cancel){
+        super(title, "dialog");
+        this.confirm = confirm;
+        content().add(text);
+        buttons().addButton("Ok", () -> {
+            confirm.run();
+            hide();
+        });
+        buttons().addButton("Cancel", () -> {
+            cancel.run();
+            hide();
+        }).width(110f);
+    }
+
 }
