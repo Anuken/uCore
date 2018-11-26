f73e538757ac66ff62d7f25d93011142b6abb8df
Anuken <arnukren@gmail.com>
2018-11-25 20:50:30 -0500

    Build fix

--- src/io/anuke/ucore/graphics/Pixmaps.java
+++ src/io/anuke/ucore/graphics/Pixmaps.java
@@ -1,7 +1,5 @@
 package io.anuke.ucore.graphics;
 
-import com.badlogic.gdx.PixmapIO;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
